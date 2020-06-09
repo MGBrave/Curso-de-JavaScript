552171134caada3f1021eb7ad1f27760a5add302
martageraldo <60014891+martageraldo@users.noreply.github.com>
2020-06-08 23:10:40 -0300

    commit full course
    Curso JavaScript

---
+++ dowloads/aula16/ambiente.js
@@ -0,0 +1,32 @@
+var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+
+
+
+
+/*
+var c = 1
+while(c <= 3){
+    console.log(`Passo ${c}`)
+    c++ // é a mesma coisa que c = c + 1
+}
+
+*/
+
+
+
+
+
+
+/*
+console.log(`Tudo Bem?`)
+console.log(`Tudo Bem?`)
+console.log(`Tudo Bem?`)
+console.log(`Tudo Bem?`)
+console.log(`Tudo Bem?`)
+*/
---
+++ dowloads/aula17/ambiente.js
@@ -0,0 +1,25 @@
+/* var c = 1
+while (c <= 5){
+    console.log(c)
+    c++
+} 
+*/
+
+/*
+var c = 1
+while(c <= 3){
+    console.log(`Passo ${c}`)
+    c++ // é a mesma coisa que c = c + 1
+}
+*/
+/*for(var c = 1; c <= 5; c++) {
+    console.log(c)
+
+}
+*/
+
+console.log(`Vai começar...`)
+for(var c = 1; c <= 4; c++) {
+    console.log(c)
+}
+console.log(`FIM!`)
